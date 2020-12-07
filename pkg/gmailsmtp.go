4eaf75015e93569d0fc96ca5fee6e908f64616eb
RyanDevlin <rdevlin.mail@gmail.com>
2020-12-06 23:23:29 -0500

    Removed html file

--- pkg/gmailsmtp.go
+++ pkg/gmailsmtp.go
@@ -21,7 +21,8 @@ func SendEmailSMTP(to []string, data interface{}, template string) (bool, error)
 
 	emailAuth = smtp.PlainAuth("", emailFrom, emailPassword, emailHost)
 
-	emailBody := "<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"utf-8\" /><meta http-equiv=\"x-ua-compatible\" content=\"ie=edge\" /><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\" /><title></title><link rel=\"stylesheet\" href=\"css/main.css\" /><link rel=\"icon\" href=\"images/favicon.png\" /></head><body><h1>Congratualtions! You won!!</h1><img src=\"http://thegalley.com/galleyblog/wp-content/uploads/sites/2/2013/12/995589_728552843822962_1742475431_n.png\" alt=\"LA\" style=\"width:100%\"><div class=\"\"><a href=\"https://www.youtube.com/watch?v=dQw4w9WgXcQ\">Only limited trme left! Click here!</a></div></body></html>"
+	emailBody := "TEST"
+	//emailBody := "<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"utf-8\" /><meta http-equiv=\"x-ua-compatible\" content=\"ie=edge\" /><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\" /><title></title><link rel=\"stylesheet\" href=\"css/main.css\" /><link rel=\"icon\" href=\"images/favicon.png\" /></head><body><h1>Congratualtions! You won!!</h1><img src=\"http://thegalley.com/galleyblog/wp-content/uploads/sites/2/2013/12/995589_728552843822962_1742475431_n.png\" alt=\"LA\" style=\"width:100%\"><div class=\"\"><a href=\"https://www.youtube.com/watch?v=dQw4w9WgXcQ\">Only limited trme left! Click here!</a></div></body></html>"
 	/*
 		emailBody, err := parseTemplate(template, data)
 		if err != nil {
@@ -30,7 +31,7 @@ func SendEmailSMTP(to []string, data interface{}, template string) (bool, error)
 	*/
 
 	mime := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\n"
-	subject := "Subject: " + "Congratulations!\n"
+	subject := "Subject: " + "TEST!\n"
 	msg := []byte(subject + mime + "\n" + emailBody)
 	addr := fmt.Sprintf("%s:%s", emailHost, emailPort)
 
